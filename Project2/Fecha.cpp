62dcf6a114538d6957ce5ff9b1854b4687e870fc
David González Córdoba <106786019+Bloodiewormer@users.noreply.github.com>
2025-09-28 01:50:04 -0600

    fix

---
+++ Project2/Fecha.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bloodie on 27/9/2025.
+//
+
+#include "Fecha.h"
---
+++ Project2/Fecha.h
@@ -0,0 +1,14 @@
+//
+// Created by Bloodie on 27/9/2025.
+//
+
+#ifndef PROYECTO_1_PROGRA1_2025_FECHA_H
+#define PROYECTO_1_PROGRA1_2025_FECHA_H
+
+
+class Fecha
+{
+};
+
+
+#endif //PROYECTO_1_PROGRA1_2025_FECHA_H
